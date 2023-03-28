d05145a00f745c91e8339d521e88c353aedc500b
IvanV <20206262@uatx.com>
2023-03-28 01:02:39 -0600

    nuevas interfaces y algunas correcciones

---
+++ app/src/main/java/com/example/ojoyem_app/buscar_especialista_lista.java
@@ -0,0 +1,14 @@
+package com.example.ojoyem_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buscar_especialista_lista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_especialista_lista);
+    }
+}
